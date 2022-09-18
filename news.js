adf9da0705619fab76c8c7892bfa118430dabd1d
IsaiahDay2000 <113795913+IsaiahDay2000@users.noreply.github.com>
2022-09-18 07:50:33 -0400

    Add first article

--- news.js
+++ news.js
@@ -1,1 +1,1 @@
-var articles = [{"title": "Testing!", "description": "Just making sure the news part works!", "file: "1"}]
+var articles = [{"title": "Testing!", "description": "Just making sure the news part works!", "file: 1}]
