ed2bfc73db2e1f91d5ba16725d2cb12e295f81a3
IsaiahDay2000 <113795913+IsaiahDay2000@users.noreply.github.com>
2022-09-18 16:04:31 -0400

    add link

--- news.js
+++ news.js
@@ -3,9 +3,12 @@ var newsItems = document.getElementById("news-items")
 
 articles.forEach(article => {
     newsItems.innerHTML += `
+    <a href="https://estrange-gov.github.io/news/${article["file"]}>
     <div style="margin-left:15px;">
     <h1>${article["title"]}</h1>
     <p>${article["description"]}
-    </div>`
+    </div>
+    </a>`
+    
 });
 
