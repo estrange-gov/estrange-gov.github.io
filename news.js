0c47fa64ac38a098a721f10d5a7d4dc136732354
IsaiahDay2000 <113795913+IsaiahDay2000@users.noreply.github.com>
2022-09-18 12:00:58 -0400

    Testing news home page

--- news.js
+++ news.js
@@ -1,1 +1,11 @@
-var articles = [{"title": "Testing!", "description": "Just making sure the news part works!", "file: 1}]
+var articles = [{"title": "Testing!", "description": "Just making sure the news part works!", "file": "1"}]
+var newsItems = document.getElementById("news-items")
+
+articles.forEach(article => {
+    const newsGet = async () => {
+        const response = await fetch('https://estrange-gov.github.io/news/'+articles[file]);
+        const responseText = await response.text
+    }
+    newsItems.innerHTML += responseText
+});
+
