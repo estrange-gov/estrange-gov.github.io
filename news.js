17320c75992e93375cfc373e65f9e3763271a867
IsaiahDay2000 <113795913+IsaiahDay2000@users.noreply.github.com>
2022-09-18 14:28:22 -0400

    Add formatting

--- news.js
+++ news.js
@@ -3,7 +3,9 @@ var newsItems = document.getElementById("news-items")
 
 articles.forEach(article => {
     newsItems.innerHTML += `
+    <div style="margin-left:15px;">
     <h1>${article["title"]}</h1>
-    <p>${article["description"]}`
+    <p>${article["description"]}
+    </div>`
 });
 
