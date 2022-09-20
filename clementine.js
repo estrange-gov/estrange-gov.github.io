bccfe81f8d22b322c35ad2a7e23fc0ccee9b2a4a
IsaiahDay2000 <113795913+IsaiahDay2000@users.noreply.github.com>
2022-09-20 07:42:15 -0400

    Update clementine counter and a few other things

--- clementine.js
+++ clementine.js
@@ -1,3 +1,17 @@
-var number = document.getElementById('number')
-
-number.innerHTML = Math.random() * (1999 - 100) + 100
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+  
+  
+  
+  document.getElementById("number").innerHTML = Math.floor(Math.random() * (1999 - 100) + 10)
+  
+  async function start() {
+    while (true) {
+      await delay(Math.random() * (4000 - 1000 + 1000));
+      document.getElementById("number").innerHTML = (parseInt(document.getElementById("number").innerHTML) + 1).toString()
+    }
+  }
+  
+  start()
+  
