db4b390fc515c5bcc0931b267f4df267ded42da7
IsaiahDay2000 <113795913+IsaiahDay2000@users.noreply.github.com>
2022-11-04 08:41:30 -0400

    Add news article 3

--- news.js
+++ news.js
@@ -1,4 +1,6 @@
-var articles = [{"title": "Grumbling among Populace over Owen's Campaign", "description": "Who will win this month's election? <small>By Isaiah Day</small>", "file": "2"}, {"title": "Testing!", "description": "Just making sure the news part works!", "file": "1"}]
+var articles = [{"title":"The Daily Estrange Email being so predictable is a major security risk.", "description": "A not to far fetched parody of The Daily Estrange Email. <small>By Isaiah Day</small>", "file": "3"},
+{"title": "Grumbling among Populace over Owen's Campaign", "description": "Who will win this month's election? <small>By Isaiah Day</small>", "file": "2"}, 
+{"title": "Testing!", "description": "Just making sure the news part works!", "file": "1"}]
 var newsItems = document.getElementById("news-items")
 
 articles.forEach(article => {
